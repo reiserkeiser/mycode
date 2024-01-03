d5cef1550fd985e9f4cb72906dc4757fe21b50c6
reiserkeiser <reiserkeiser@gmail.com>
2024-01-03 18:05:41 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# displaying only IP addresses
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
+
